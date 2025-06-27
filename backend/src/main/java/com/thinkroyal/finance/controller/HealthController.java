0639855d71775a1681763f4e4b255184a8b84df5
thinktoyal <thinkroyal@icloud.com>
2025-06-27 04:57:42 -0400

    Initial commit

---
+++ backend/src/main/java/com/thinkroyal/finance/controller/HealthController.java
@@ -0,0 +1,12 @@
+package main.java.com.thinkroyal.finance.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping
+    public String health() {
+        return "OK";
+    }
+}
